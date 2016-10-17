119e0425056b2f701207a181cb3c4d0f4ebe0e09
Carlos <csilva2810@gmail.com>
2016-10-17 16:39:09 +0000

    Atualizando demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,69 +1,64 @@
 (function () {
+    
     "use strict";
     var btnUpload   = document.querySelector("#btn-upload"),
         fileElement = document.querySelector("#file-element"),
         themeLight  = document.querySelector("#theme-light"),
         themeDark   = document.querySelector("#theme-dark"),
-        colorGreen  = document.querySelector("#btn-green"),
-        colorBlue   = document.querySelector("#btn-blue"),
-        colorRed    = document.querySelector("#btn-red"),
-        colorPink   = document.querySelector("#btn-pink"),
-        htmlTag     = document.querySelector('html'),
+        btnsTheme   = document.querySelectorAll('.theme-item-btn'),
+        btnsColor   = document.querySelectorAll('.btn-color'),
         
         setLocalStorage = function (item, value) {
-            if (localStorage) {
+            if (window.hasOwnProperty('localStorage')) {
                 localStorage.setItem(item, value);
-            } else {
-                return "";
             }
         },
 
         getLocalStorage = function (item) {
-            if (localStorage.getItem(item)) {
+            if (window.hasOwnProperty('localStorage')) {
                 return localStorage.getItem(item);
             }
-
-            return "";
         },
 
         setTheme = function (theme) {
-                        
+            
             theme = theme || "light";
-            htmlTag.className = htmlTag.className.replace(/dark|light/g, "");
-            htmlTag.className += " "+theme;
-
+            document.body.classList.remove('dark', 'light');
+            document.body.classList.add(theme);
             setLocalStorage("theme", theme);
+            
         },
 
         setColor = function (color) {
 
             color = color || "green";
-            htmlTag.className = htmlTag.className.replace(/blue|red|green|pink/g, "");
-            htmlTag.className += " "+color;
-
+            document.body.className = document.body.className.replace(/(blue|red|green|pink|purple|cyan|teal|yellow|orange)/g, '');
+            document.body.classList.add(color);
             setLocalStorage("color", color);
+
         };
 
-    if (localStorage) {
+    if (window.hasOwnProperty('localStorage')) {
         setTheme(getLocalStorage('theme'));
         setColor(getLocalStorage('color'));
     }
 
     btnUpload.addEventListener("click", function (e) {
+        
+        e.stopPropagation();
+        e.preventDefault();
+        
         if (fileElement) {
             fileElement.click();
         }
-
-        e.stopPropagation();
-        e.preventDefault();
+        
     }, false);
 
     fileElement.addEventListener("change", function () {
-        var i,
-            len = this.files.length;
 
         Player.clearPlayList();
-        for (i = 0; i < len; i += 1) {
+
+        for (var i = 0, len = this.files.length; i < len; i += 1) {
             Player.setPlayList(this.files[i]);
         }
 
@@ -72,28 +67,18 @@
 
     }, false);
 
-    themeLight.addEventListener("click", function () {
-        setTheme("light");
-    }, false);
+    btnsTheme.forEach(function (btnTheme) {
+        btnTheme.addEventListener('click', function () {
+            var themeScheme = this.getAttribute('data-theme-scheme');
+            setTheme(themeScheme);
+        }, false);
+    });
 
-    themeDark.addEventListener("click", function () {
-        setTheme("dark");
-    }, false);
-
-    colorGreen.addEventListener("click", function () {
-        setColor("green");
-    }, false);
-
-    colorBlue.addEventListener("click", function () {
-        setColor("blue");
-    }, false);
-
-    colorRed.addEventListener("click", function () {
-        setColor("red");
-    }, false);
-
-    colorPink.addEventListener("click", function () {
-        setColor("pink");
-    }, false);
+    btnsColor.forEach(function (btnColor) {
+        btnColor.addEventListener('click', function () {
+            var color = this.getAttribute('data-theme-color');
+            setColor(color);
+        }, false);
+    });
 
 }());
