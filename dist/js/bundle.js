a96f4a690345df802206b651713d1a023e3c3727
Carlos Silva <csilva2810@gmail.com>
2016-10-19 00:01:45 -0200

    gulp browserify

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,17 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
 var toggles = document.querySelectorAll('.dropdown-toggle');
 var dropdowns = [];
 
-var hasClass = function (el, cl) {
-	return (el.classList.toString().indexOf(cl) > -1) ? true : false;
-}
+var hasClass = function hasClass(el, cl) {
+	return el.classList.toString().indexOf(cl) > -1 ? true : false;
+};
 
 if (toggles) {
 	toggles.forEach(function (toggle) {
 		toggle.addEventListener('click', function (e) {
 
 			e.stopPropagation();
-			
+
 			var target = this.getAttribute('data-dropdown');
 			if (!target) return false;
 
@@ -27,7 +29,6 @@ if (toggles) {
 					e.stopPropagation();
 				}, false);
 			}
-
 		}, false);
 	});
 
@@ -39,66 +40,67 @@ if (toggles) {
 				dropdowns.splice(dropdowns.indexOf(dropdown), 1);
 			}
 		});
-
 	}, false);
-
 }
+
 },{}],2:[function(require,module,exports){
+"use strict";
+
 var timeHelper = require('../helpers/time');
 
-var audio           = document.querySelector("#player"),
-    btnPlay         = document.querySelector("#btn-play"),
-    btnPause        = document.querySelector("#btn-pause"),
-    btnPrev         = document.querySelector("#prev"),
-    btnNext         = document.querySelector("#next"),
-    btnRepeat       = document.querySelector("#repeat"),
-    btnRandom       = document.querySelector("#random"),
-    volumeControl   = document.querySelector("#volume"),
-    timeLine        = document.querySelector("#timeline"),
-    musicTimeCount  = document.querySelector("#time-count"),
-    musicTime       = document.querySelector("#time"),
-    musicName       = document.querySelector("#music-name"),
+var audio = document.querySelector("#player"),
+    btnPlay = document.querySelector("#btn-play"),
+    btnPause = document.querySelector("#btn-pause"),
+    btnPrev = document.querySelector("#prev"),
+    btnNext = document.querySelector("#next"),
+    btnRepeat = document.querySelector("#repeat"),
+    btnRandom = document.querySelector("#random"),
+    volumeControl = document.querySelector("#volume"),
+    timeLine = document.querySelector("#timeline"),
+    musicTimeCount = document.querySelector("#time-count"),
+    musicTime = document.querySelector("#time"),
+    musicName = document.querySelector("#music-name"),
     playListElement = document.querySelector("#play-list"),
-    loading         = document.querySelector("#loading-music"),
-    flip            = document.querySelector("#flip-container"),
-    rd              = new FileReader();
+    loading = document.querySelector("#loading-music"),
+    flip = document.querySelector("#flip-container"),
+    rd = new FileReader();
 
 var Player = {
-    currentTrack : 0,
-    isPlaying    : false,
-    isRepeating  : false,
-    isRandomized : false,
-    playList     : []
+    currentTrack: 0,
+    isPlaying: false,
+    isRepeating: false,
+    isRandomized: false,
+    playList: []
 };
 
-var setPlayList = function (music) {
+var setPlayList = function setPlayList(music) {
     Player.playList.push(music);
-}
+};
 
-var activateListItem = function (id) {
+var activateListItem = function activateListItem(id) {
     document.querySelector("#list-item-" + id).classList.add("is-active");
     document.querySelector("#list-icon-" + id).classList.add("is-active");
-}
+};
 
-var inactivateListItem = function (id) {
+var inactivateListItem = function inactivateListItem(id) {
     document.querySelector("#list-item-" + id).classList.remove("is-active");
     document.querySelector("#list-icon-" + id).classList.remove("is-active");
-}
+};
 
-var clearPlayList = function () {
+var clearPlayList = function clearPlayList() {
     Player.playList = [];
-}
+};
 
-var showLoading = function () {
+var showLoading = function showLoading() {
     loading.classList.add("show");
 };
 
-var hideLoading = function () {
+var hideLoading = function hideLoading() {
     loading.classList.remove("show");
 };
 
-var playMusic = function (track) {
-    
+var playMusic = function playMusic(track) {
+
     rd.onloadstart = function () {
         showLoading();
     };
@@ -109,78 +111,73 @@ var playMusic = function (track) {
 
     rd.readAsDataURL(Player.playList[track]);
 
-    audio.onloadeddata = function() {
-        
+    audio.onloadeddata = function () {
+
         audio.play();
-        
+
         flip.classList.add('is-flipped');
         if (Player.currentTrack > -1) {
-           inactivateListItem(Player.currentTrack);
+            inactivateListItem(Player.currentTrack);
         }
-        
+
         activateListItem(track);
-        
+
         Player.isPlaying = true;
         Player.currentTrack = track;
-        
+
         hideLoading();
         setMusicName(Player.playList[track].name);
         setTimeLineMax(this.duration);
         setMusicTime(this.duration);
     };
-    
 };
 
-var pause = function () {
-    
+var pause = function pause() {
+
     if (audio.src == "") return false;
 
     flip.classList.remove('is-flipped');
     inactivateListItem(Player.currentTrack);
-    
+
     Player.isPlaying = false;
     audio.pause();
-    
 };
 
-var resume = function () {
-    
+var resume = function resume() {
+
     if (audio.src == "") return false;
 
     flip.classList.add('is-flipped');
     activateListItem(Player.currentTrack);
-    
+
     Player.isPlaying = true;
     audio.play();
-    
-}
-
-var playPrev = function () {
-    
-    if (audio.src == "") return false;
-    
-    var prev = Player.currentTrack - 1;
-    (prev > -1) ? playMusic(prev) : playMusic(0);
-    
 };
 
-var playNext = function () {
-    
+var playPrev = function playPrev() {
+
     if (audio.src == "") return false;
-    
-    var next      = Player.currentTrack + 1;
+
+    var prev = Player.currentTrack - 1;
+    prev > -1 ? playMusic(prev) : playMusic(0);
+};
+
+var playNext = function playNext() {
+
+    if (audio.src == "") return false;
+
+    var next = Player.currentTrack + 1;
     var lastMusic = Player.playList.length - 1;
-    var random    = Math.round( Math.random() * lastMusic );
+    var random = Math.round(Math.random() * lastMusic);
 
     if (!Player.isRandomized) {
-        (next <= lastMusic) ? playMusic(next) : playMusic(0);
+        next <= lastMusic ? playMusic(next) : playMusic(0);
     } else {
         playMusic(random);
     }
-    
 };
 
-var repeat = function () {
+var repeat = function repeat() {
     if (!Player.isRepeating) {
         btnRepeat.classList.add('is-on');
         audio.setAttribute("loop", "");
@@ -192,7 +189,7 @@ var repeat = function () {
     }
 };
 
-var randomize = function () {
+var randomize = function randomize() {
     if (!Player.isRandomized) {
         btnRandom.classList.add('is-on');
         Player.isRandomized = true;
@@ -202,70 +199,73 @@ var randomize = function () {
     }
 };
 
-var changeVolume = function () {
+var changeVolume = function changeVolume() {
     audio.volume = volumeControl.value / 10;
 };
 
-var changeTime = function () {
+var changeTime = function changeTime() {
     if (audio.readyState != 0) audio.currentTime = timeLine.value;
 };
 
-var timeLineUpdate = function () {
+var timeLineUpdate = function timeLineUpdate() {
     timeLine.value = audio.currentTime;
 };
 
-var setTimeLineMax = function (time) {
-    timeLine.setAttribute( "max", Math.round(time) );
+var setTimeLineMax = function setTimeLineMax(time) {
+    timeLine.setAttribute("max", Math.round(time));
 };
 
-var setMusicTime = function (time) {
-    musicTime.innerHTML = timeHelper.secondsToTime( Math.round(time) );
+var setMusicTime = function setMusicTime(time) {
+    musicTime.innerHTML = timeHelper.secondsToTime(Math.round(time));
 };
 
-var musicCountUpdate = function (time) {
-    musicTimeCount.innerHTML = timeHelper.secondsToTime( Math.round(time) );
+var musicCountUpdate = function musicCountUpdate(time) {
+    musicTimeCount.innerHTML = timeHelper.secondsToTime(Math.round(time));
 };
 
-var setMusicName = function (name) {
+var setMusicName = function setMusicName(name) {
     musicName.innerHTML = name.replace(".mp3", "");
 };
 
-var createPlayList = function () {
-    
-    var musicName, i, li, button, span,
+var createPlayList = function createPlayList() {
+
+    var musicName,
+        i,
+        li,
+        button,
+        span,
         len = Player.playList.length;
 
     playListElement.innerHTML = "";
 
     for (i = 0; i < len; i += 1) {
-        
+
         musicName = document.createTextNode(Player.playList[i].name.replace(".mp3", ""));
 
-        li     = document.createElement("li");
+        li = document.createElement("li");
         button = document.createElement("button");
-        span   = document.createElement("span");
+        span = document.createElement("span");
 
         button.setAttribute("type", "button");
         button.setAttribute("class", "btn btn-circle btn-small");
         button.innerHTML = '<i class="icon icon-play"></i>';
-        
+
         (function (id) {
-            button.addEventListener('click', function () { 
+            button.addEventListener('click', function () {
                 playMusic(id);
             }, false);
         })(i);
 
-        span.setAttribute("id", "list-icon-"+i);
+        span.setAttribute("id", "list-icon-" + i);
         span.setAttribute("class", "sound-wave");
         span.innerHTML = "<span class='bar'></span><span class='bar'></span><span class='bar'></span>";
-    
-        li.setAttribute("id", "list-item-"+i);
+
+        li.setAttribute("id", "list-item-" + i);
         li.setAttribute("class", "list-item");
         li.appendChild(button);
         li.appendChild(musicName);
         li.appendChild(span);
         playListElement.appendChild(li);
-
     };
 };
 
@@ -282,18 +282,18 @@ volumeControl.addEventListener("change", changeVolume, false);
 
 //timeLine
 timeLine.addEventListener("change", changeTime, false);
-timeLine.addEventListener("mousedown", function() {
+timeLine.addEventListener("mousedown", function () {
     audio.removeEventListener("timeupdate", timeLineUpdate);
 }, false);
-timeLine.addEventListener("mouseup", function() {
+timeLine.addEventListener("mouseup", function () {
     audio.addEventListener("timeupdate", timeLineUpdate, false);
 }, false);
 
 //player
 audio.addEventListener("ended", playNext, false);
 audio.addEventListener("timeupdate", timeLineUpdate, false);
-audio.addEventListener("timeupdate", function() { 
-    musicCountUpdate( Math.floor(this.currentTime) );
+audio.addEventListener("timeupdate", function () {
+    musicCountUpdate(Math.floor(this.currentTime));
 }, false);
 
 module.exports = {
@@ -301,21 +301,24 @@ module.exports = {
     clearPlayList: clearPlayList,
     playMusic: playMusic,
     createPlayList: createPlayList
-}
+};
+
 },{"../helpers/time":3}],3:[function(require,module,exports){
+'use strict';
+
 function padLeft(n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-var secondsToTime = function (seconds) {
+var secondsToTime = function secondsToTime(seconds) {
 
   if (isNaN(seconds) || seconds == "" || typeof seconds != 'number') return "00:00";
 
-  var hours   = parseInt( seconds / 3600 ) % 24,
-      minutes = parseInt( seconds / 60 ) % 60,
-      seconds = parseInt( seconds % 60);
+  var hours = parseInt(seconds / 3600) % 24,
+      minutes = parseInt(seconds / 60) % 60,
+      seconds = parseInt(seconds % 60);
 
   if (hours > 0) {
     var result = padLeft(hours, 2, 0) + ':' + padLeft(minutes, 2, 0) + ':' + padLeft(seconds, 2, 0);
@@ -324,55 +327,51 @@ var secondsToTime = function (seconds) {
   }
 
   return result;
-
 };
 
 module.exports = {
   secondsToTime: secondsToTime
-}
+};
+
 },{}],4:[function(require,module,exports){
+'use strict';
+
 require('./components/dropdown.js');
 
 var Player = require('./components/player.js');
 
 (function () {
-    
-    var btnUpload   = document.querySelector("#btn-upload"),
+
+    var btnUpload = document.querySelector("#btn-upload"),
         fileElement = document.querySelector("#file-element"),
-        themeLight  = document.querySelector("#theme-light"),
-        themeDark   = document.querySelector("#theme-dark"),
-        btnsTheme   = document.querySelectorAll('.theme-item-btn'),
-        btnsColor   = document.querySelectorAll('.btn-color'),
-        
-        setLocalStorage = function (item, value) {
-            if (window.hasOwnProperty('localStorage')) {
-                localStorage.setItem(item, value);
-            }
-        },
+        themeLight = document.querySelector("#theme-light"),
+        themeDark = document.querySelector("#theme-dark"),
+        btnsTheme = document.querySelectorAll('.theme-item-btn'),
+        btnsColor = document.querySelectorAll('.btn-color'),
+        setLocalStorage = function setLocalStorage(item, value) {
+        if (window.hasOwnProperty('localStorage')) {
+            localStorage.setItem(item, value);
+        }
+    },
+        getLocalStorage = function getLocalStorage(item) {
+        if (window.hasOwnProperty('localStorage')) {
+            return localStorage.getItem(item);
+        }
+    },
+        setTheme = function setTheme(theme) {
 
-        getLocalStorage = function (item) {
-            if (window.hasOwnProperty('localStorage')) {
-                return localStorage.getItem(item);
-            }
-        },
+        theme = theme || "light";
+        document.body.classList.remove('dark', 'light');
+        document.body.classList.add(theme);
+        setLocalStorage("theme", theme);
+    },
+        setColor = function setColor(color) {
 
-        setTheme = function (theme) {
-            
-            theme = theme || "light";
-            document.body.classList.remove('dark', 'light');
-            document.body.classList.add(theme);
-            setLocalStorage("theme", theme);
-            
-        },
-
-        setColor = function (color) {
-
-            color = color || "green";
-            document.body.className = document.body.className.replace(/(blue|red|green|pink|purple|cyan|teal|yellow|orange)/g, '');
-            document.body.classList.add(color);
-            setLocalStorage("color", color);
-
-        };
+        color = color || "green";
+        document.body.className = document.body.className.replace(/(blue|red|green|pink|purple|cyan|teal|yellow|orange)/g, '');
+        document.body.classList.add(color);
+        setLocalStorage("color", color);
+    };
 
     if (window.hasOwnProperty('localStorage')) {
         setTheme(getLocalStorage('theme'));
@@ -380,14 +379,13 @@ var Player = require('./components/player.js');
     }
 
     btnUpload.addEventListener("click", function (e) {
-        
+
         e.stopPropagation();
         e.preventDefault();
-        
+
         if (fileElement) {
             fileElement.click();
         }
-        
     }, false);
 
     fileElement.addEventListener("change", function () {
@@ -400,7 +398,6 @@ var Player = require('./components/player.js');
 
         Player.createPlayList();
         Player.playMusic(0);
-
     }, false);
 
     btnsTheme.forEach(function (btnTheme) {
@@ -416,9 +413,13 @@ var Player = require('./components/player.js');
             setColor(color);
         }, false);
     });
+})();
 
-}());
 },{"./components/dropdown.js":1,"./components/player.js":2}],5:[function(require,module,exports){
+'use strict';
+
 require('./index.js');
-},{"./index.js":4}]},{},[5])
-//# sourceMappingURL=bundle.js.map
+
+},{"./index.js":4}]},{},[5]);
+
+//# sourceMappingURL=maps/bundle.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,57 @@
 'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+var babelify   = require('babelify'),
+    browserify = require('browserify'),
+    buffer     = require('vinyl-buffer'),
+    gulp       = require('gulp'),
+    gutil      = require('gulp-util'),
+    sass       = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
+    merge      = require('merge'),
+    rename     = require('gulp-rename'),
+    source     = require('vinyl-source-stream'),
+    sourceMaps = require('gulp-sourcemaps'),
+    watchify   = require('watchify');
+
+var config = {
+  js: {
+    src: './src/js/main.js',
+    outputDir: './dist/js/', 
+    mapDir: './maps/',
+    outputFile: 'bundle.js'
+  },
+  css: {
+    src: './sass/**/*.scss',
+    outputDir: './dist/css/',
+    outputFile: 'main.css'
+  }
+};
+
+function bundle (bundler) {
+
+  return bundler
+    .bundle() // Start bundle
+    .pipe(source(config.js.src)) // Entry point
+    .pipe(buffer()) // Convert to gulp pipeline
+    .pipe(rename(config.js.outputFile)) // Rename output from 'main.js' to 'bundle.js'
+    .pipe(sourceMaps.init({ loadMaps : true })) // Strip inline source maps
+    .pipe(sourceMaps.write(config.js.mapDir)) // Save source maps to their own directory
+    .pipe(gulp.dest(config.js.outputDir)) // Save 'bundle' to build/
+    .pipe(livereload()); // Reload browser if relevant
+
+}
+
+gulp.task('bundle', function () {
+  var bundler = browserify(config.js.src) // Pass browserify the entry point
+                  .transform(babelify, { presets : [ 'es2015' ] }); // Then, babelify, with ES2015 preset
+
+  return bundle(bundler); // Chain other options -- sourcemaps, rename, etc.
+});
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src(config.css.src)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest(config.css.outputDir));
 });
  
 gulp.task('sass:watch', function () {
